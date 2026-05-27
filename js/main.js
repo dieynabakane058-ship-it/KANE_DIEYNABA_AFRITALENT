30ba3f90f5634a8a32bdd2f51e327a58ea32f302
dieynabakane058-ship-it <dieynabakane058@gmail.com>
2026-05-27 17:00:35 +0000

    JS : filtrage dynamique des freelances et validation du formulaire de contact

--- js/main.js
+++ js/main.js
@@ -94,3 +94,91 @@ const observer = new IntersectionObserver((entries) => {
 /*pour chaque elemen ".hidden"*/
 /*on demande a l'observer de le surveiler*/
 hiddenElements.forEach((el) =>observer.observe(el));
+
+
+
+/*boutons freelances*/
+const filterButtons = document.querySelectorAll(".filter-btn");
+/*selection des cartes*/
+const cards = document.querySelectorAll(".freelance-card");
+filterButtons.forEach((button) =>{
+    button.addEventListener("click" , () => {
+        /*recuperation de la categories*/
+        const filter = button.getAttribute("data-filter");
+        cards.forEach((card) =>{
+            if(filter === "all"){
+                card.parentElement.style.display = "block"; 
+            }
+            /*afficher seulement la bonne categories*/
+            else if (card.classList.contains(filter)){
+                card.parentElement.style.display = "block";
+            }
+            /*cacher les autres cartes*/
+            else{
+                card.parentElement.style.display = "none"
+            }
+        });
+    });
+});
+
+
+/*formulaire*/
+const form = document.getElementById("contactForm");
+form.addEventListener("submit" , function(e){
+    /*pour empecher l'envoie automatique*/
+    e.preventDefault();
+    /*recuperation des champs*/
+    const nom = document.getElementById("nom").Value.trim();
+    const email = document.getElementById("email").Value.trim();
+    const sujet = document.getElementById("sujet").Value;
+    const message = document.getElementById("message").Value.trim();
+    /*recuperation des zones erreur*/
+const nomError = document.getElementById("nomError");
+const emailError = document.getElementById("emailError");
+const sujetError = document.getElementById("sujetError");
+const messageError = document.getElementById("messageError");
+const successMessage = document.getElementById("successMessage");
+    /*reset erreur*/
+nomError.textContent = "";
+emailError.textContent = "";
+sujetError.textContent = "";
+messageError.textContent = "";
+successMessage.textContent = "";
+
+    /*variable de la validation*/
+let isvalid = true;
+
+    /*nom*/
+if(nom === ""){
+    nomError.textContent = "le nom est obligatoire";
+    isvalid = false;
+}
+    /*email*/
+ const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+ if(email === ""){
+emailError.textContent = "L'email est obligatoire";
+ isValid = false;
+ }
+else if(!emailRegex.test(email)){
+emailError.textContent = "Email invalide";
+isValid = false;
+    }
+    /*sujet*/
+ if(sujet === ""){
+ sujetError.textContent = "Choisissez un sujet";
+isValid = false;
+    }
+    /*message*/
+if(message.length < 20){
+messageError.textContent =
+"Le message doit contenir au moins 20 caractères";
+isValid = false;
+    }
+    /*succes*/
+if(isValid){
+ successMessage.textContent =
+"Message envoyé avec succès ✅";
+form.reset();
+
+ }
+});
